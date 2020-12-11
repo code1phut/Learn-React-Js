093a1dee00f910f2f8e767ec81c50b44abd82bb6
anhnq <anh.nguyen1@tda.company>
2020-12-11 16:57:38 +0700

    State

--- lesson-6-refs/src/App.js
+++ lesson-6-refs/src/App.js
@@ -1,50 +1,55 @@
 import React, { Component } from 'react';
-import Product from "./Components/Product";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
 
-    onAddProduct = () => {
-        console.log(this.refs.name.value);
+        this.state = {
+            products : [
+                {
+                    id: 1,
+                    name: 'Iphone 12 Pro max',
+                    price: 15000000,
+                    image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021660000',
+                    status: true
+                },
+                {
+                    id: 2,
+                    name: 'Iphone 12',
+                    price: 14000000,
+                    image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021660000',
+                    status: true,
+                },
+                {
+                    id: 3,
+                    name: 'Iphone 11 pro max',
+                    price: 13000000,
+                    image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021660000',
+                    status: true
+                }
+            ],
+            isActive: true
+        }
     }
-    onClickMe = () => {
-        console.log('Day la app Components');
+
+    onSetState = () => {
+        console.log(true)
+            this.setState({
+                isActive: !this.state.isActive
+            })
     }
     render() {
 
-        var products = [
-            {
-                id: 1,
-                name: 'Iphone 12 Pro max',
-                price: 15000000,
-                image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021660000',
-                status: true
-            },
-            {
-                id: 2,
-                name: 'Iphone 12',
-                price: 14000000,
-                image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021660000',
-                status: true,
-            },
-            {
-                id: 3,
-                name: 'Iphone 11 pro max',
-                price: 13000000,
-                image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021660000',
-                status: true
-            }
-        ];
-
-        var elements = products.map((product, index) => {
+        var elements = this.state.products.map((product, index) => {
             var result = '';
             if (product.status) {
-                result = <Product
-                    key={product.id}
-                    price={product.price}
-                    image={product.image}
-                >
-                    {product.name}
-                </Product>
+                result = <tr key={product.id}>
+                    <td>{index}</td>
+                    <td>{product.name}</td>
+                    <td>
+                        <label className='label label-success'>{product.price}VND</label>
+                    </td>
+                </tr>
             }
 
             return  result;
@@ -54,35 +59,27 @@ class App extends Component {
             <div>
                 <nav className="navbar navbar-inverse">
                     <div className="container-fluid">
-                        <a className="navbar-brand">Event - Refs</a>
+                        <a className="navbar-brand">State</a>
                     </div>
                 </nav>
 
                 <div className='container'>
                     <div className='row'>
-                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <div className="panel panel-primary">
-                                <div className="panel-heading">
-                                    <h3 className="panel-title">Them San Pham</h3>
-                                </div>
-                                <div className="panel-body">
-                                    <div className="form-group">
-                                        <label>Ten San Pham</label>
-                                        <input type="text" className="form-control" name="" ref="name"/>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary" onClick={this.onAddProduct}>Luu</button>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className='col-lg-12 col-sm-12 col-md-12 col-lg-12'>
-                            { elements }
-                        </div>
-                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <button type="button" className='btn btn-primary'
-                                    onClick={ this.onClickMe }
-                            >
-                                Click Me
+                        <div className='row'>
+                            <table className="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>STT</th>
+                                    <th>Tên Sản Phẩm</th>
+                                    <th>Giá</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {elements}
+                                </tbody>
+                            </table>
+                            <button type="button" className='btn btn-primary' onClick={ this.onSetState }>
+                                Active{ this.state.isActive === true ? 'true' : 'false'}
                             </button>
                         </div>
                     </div>
